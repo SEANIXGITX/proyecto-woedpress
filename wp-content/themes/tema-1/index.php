0fda948a93698e9141055e1e1e906ca8472235b3
Seanix <seanixgx@gmail.com>
2019-02-28 10:41:02 -0400

    se hizo uso de loop en wordpress terminando el video 8 y se creo el primer card

--- wp-content/themes/tema-1/index.php
+++ wp-content/themes/tema-1/index.php
@@ -4,57 +4,23 @@
     <div class="container my-5">
       <div class="row">
         <!-- articulo -->
-        <div class="col-12 col-sm-6 col-md-4 mb-3">
-          <div class="card">
-            <img src="images/articulo1.png" class="card-img-top img-fluid" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div class="card-footer">
-              <small class="te-muted">Fecha / Categorias / Autor</small>
-            </div>
-          </div>
-        </div>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        
 
         <div class="col-12 col-sm-6 col-md-4 mb-3">
           <div class="card">
             <img src="images/articulo1.png" class="card-img-top img-fluid" alt="Card image cap">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href=" <?php the_permalink(); ?> "><h5 class="card-title"> <?php the_title(); ?> </h5></a>
+              <p class="card-text"> <?php the_excerpt(); ?> </p>
             </div>
             <div class="card-footer">
-              <small class="te-muted">Fecha / Categorias / Autor</small>
+              <small class="te-muted"> <?php echo get_the_date(); ?> / <?php the_category(', '); ?> / <?php the_author(); ?>   </small>
             </div>
           </div>
         </div>
 
-        <div class="col-12 col-sm-6 col-md-4 mb-3">
-          <div class="card">
-            <img src="images/articulo1.png" class="card-img-top img-fluid" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card Nueva</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div class="card-footer">
-              <small class="te-muted">Fecha / Categorias / Autor</small>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-12 col-sm-6 col-md-4 mb-3">
-          <div class="card">
-            <img src="images/articulo1.png" class="card-img-top img-fluid" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div class="card-footer">
-              <small class="te-muted">Fecha / Categorias / Autor</small>
-            </div>
-          </div>
-        </div>
+        <?php endwhile; endif; ?>
       </div>
     </div>
     <!-- fin contenido -->
